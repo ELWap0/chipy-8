b83080ed4740bb50379cc0d3308fe2655ea44e1b
Sergio Madrid <smadrid062@gmail.com>
2025-11-13 22:25:47 -0700

    falling asleep

--- src/chipy-8/chipy_8.py
+++ src/chipy-8/chipy_8.py
@@ -1,3 +1,4 @@
+import random
 from functools import wraps
 
 from chipy_8 import audio, graphics
@@ -5,6 +6,8 @@ from chipy_8 import audio, graphics
 MEM_SIZE = 4096
 REG_NUM = 16
 STACK_SIZE = 48
+valid_inputs = ['0', '1', '2', '3' '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+
 
 
 class chip8(graphics, audio):
@@ -12,18 +15,34 @@ class chip8(graphics, audio):
     v: bytearray = bytearray(REG_NUM)
     stack: bytearray = bytearray(STACK_SIZE)
     delay_counter: int
+    sound_counter: int
+    rom_path: str
 
-    def timer(self):
+    def __init__(self, rom):
+        self.rom_path = rom
+        graphics.setup()
+        audio.setup()
+        setupInput()
+
+
+    def delay_timer(self):
         for x in range(self.delay_counter):
             self.delay_counter = x
             yield
 
-    def soundTimer(self, count):
-        for _ in range(count):
+    def sound_timer(self):
+        for i in range(self.sound_counter):
+            if i == 1:
+                audio.play_sound()
             yield
 
     def get_input(self):
-        pass
+        user_input = input() 
+        if user_input in valid_inputs:
+            return (True, user_input)
+        else:
+            return (False, user_input)
+        
 
     def decode(self):
         op = 0xF000 & self.opcode >> 12
@@ -94,7 +113,7 @@ class chip8(graphics, audio):
                 op = self.opcode & 0x00FF
                 match op:
                     case 0x07:
-                        operation = self.get_dealy
+                        operation = self.get_delay
                     case 0x0A:
                         operation = self.get_key
                     case 0x15:
@@ -119,7 +138,7 @@ class chip8(graphics, audio):
 
         @wraps(func)
         def wrapper(self, *args, **kwargs):
-            x = self.opcode & 0x0F00 >> 8
+            x = bytes(self.opcode & 0x0F00 >> 8)
             return func(self, x, *args, **kwargs)
 
         return wrapper
@@ -129,7 +148,7 @@ class chip8(graphics, audio):
 
         @wraps(func)
         def wrapper(self, *args, **kwargs):
-            y = self.opcode & 0x00F0 >> 4
+            y = bytes(self.opcode & 0x00F0 >> 4)
             return func(self, y, *args, **kwargs)
 
         return wrapper
@@ -138,7 +157,7 @@ class chip8(graphics, audio):
     def get_nnn(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
-            nnn = self.opcode & 0x0FFF
+            nnn = bytes(self.opcode & 0x0FFF)
             return func(self, nnn, *args, **kwargs)
 
         return wrapper
@@ -147,13 +166,32 @@ class chip8(graphics, audio):
     def get_nn(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
-            nn = self.opcode & 0x00FF
+            nn = bytes(self.opcode & 0x00FF)
             return func(self, nn, *args, **kwargs)
 
         return wrapper
 
+    @staticmethod
+    def get_n(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            nnn = bytes(self.opcode & 0x000F)
+            return func(self, nnn, *args, **kwargs)
+
+        return wrapper
+
+    def ret(self):
+        self.pc = self.stack[self.sp]
+        self.sp -= 1
+
     @get_nnn
     def goto(self, nnn):
+        self.pc = nnn
+
+    @get_nnn
+    def call_sub(self,nnn):
+        self.sp += 1
+        self.stack[self.sp] = self.pc
         self.pc = nnn
 
     @get_x
@@ -162,6 +200,112 @@ class chip8(graphics, audio):
         return nn == self.v[x]
 
     @get_x
+    @get_y
+    def eqVV(self, x, y):
+        return self.v[y] != self.v[x]
+
+    @get_x
     @get_nn
-    def neqV(self, x, nn):
-        return nn != self.v[x]
+    def assigV(self, x, nn):
+        self.v[x] = nn
+
+    @get_x
+    @get_y
+    def orVV(self, x, y):
+        self.v[x] |= self.v[y]
+
+    @get_x
+    @get_y
+    def andVV(self, x, y):
+        self.v[x] &= self.v[y]
+
+    @get_x
+    @get_y
+    def xorVV(self, x, y):
+        self.v[x] ^= self.v[y]
+
+    @get_x
+    @get_y
+    def add_assigVV(self, x, y):
+        self.v[x] += self.v[y]
+
+    @get_x
+    @get_y
+    def sub_assigVV(self, x, y):
+        self.v[x] -= self.v[y]
+
+    @get_x
+    def l_shiftV(self, x):
+        self.v[x] <<= 1
+
+    @get_x
+    @get_y
+    def sub_flipVV(self, x, y):
+        self.v[x] = self.v[y] - self.v[x]
+
+    @get_x
+    def r_shiftV(self, x):
+        self.v[x] >>= 1
+
+    @get_x
+    @get_y
+    def neqVV(self, x, y):
+        return self.v[x] != self.v[y]
+
+    @get_nnn
+    def assignI(self, nnn):
+        self.I = nnn
+
+    @get_nnn
+    def jump(self, nnn):
+        self.pc = self.v[0] & nnn
+
+    @get_nn
+    def rand(self, x, nn):
+        self.v[x] = random.randbytes(1) & nn
+
+    @get_x
+    @get_y
+    @get_n
+    def draw(self, x, y, n):
+        pass
+
+    def eqKeyV(self):
+        key = self.get_input()
+        pass
+
+    def neqKeyV(self):
+        pass
+
+    def get_delay(self):
+        return self.delay_counter
+
+    def get_key(self):
+        return self.key
+
+    @get_x
+    def setDelayTimer(self, x):
+        self.delay_counter = self.v[x]
+        return self.delay_timer
+
+    @get_x
+    def setSoundTimer(self, x):
+        self.sound_timer = self.v[x]
+        return self.sound_timer
+
+    @get_x
+    def sum_assignI(self, x):
+        self.I += self.v[x]
+
+    @get_x
+    def assign_spriteI(self, x):
+        self.I += self.sprite_addr[x]
+
+    def set_BCD(self):
+        pass
+
+    def dump(self):
+        pass
+
+    def load(self):
+        pass
